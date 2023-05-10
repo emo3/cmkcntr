06d7229e6cddf0ed0b7c3076a0f05b7e0c3eaa9c
Ed Overton <e.overton@gmail.com>
2023-05-09 21:03:00 -0400

    remove unused scripts

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
 
-package 'podman'
+package %w(podman jq)
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{node['cmk']['server_file']}" do
   source "#{node['cmk']['media_url']}/#{node['cmk']['server_file']}"
@@ -68,96 +68,22 @@ end
 #   end
 # end
 
-package 'jq'
 # List rule - curl
 template '/usr/local/bin/listrulec.sh' do
   source 'listrulec.sh.erb'
   mode '0755'
   variables(
     cmkserver: 'localhost',
+    cmksite: node['cmk']['site_name'],
     apitoken: node['cmk']['secret']
   )
   sensitive true
   notifies :run, 'execute[test_api]', :immediately
 end
 
-# Create host - curl
-template '/usr/local/bin/addhostc.sh' do
-  source 'addhostc.sh.erb'
-  mode '0755'
-  variables(
-    cmkserver: 'localhost',
-    apitoken: node['cmk']['secret'],
-    host_ip: 'localhost',
-    host_name: 'cmkpod-bento-almalinux-8.vagrantup.com'
-  )
-  sensitive true
-end
-
-package %w(python3 python3-requests python3-urllib3)
-# Create host - requests
-template '/usr/local/bin/addhostr.py' do
-  source 'addhostr.py.erb'
-  mode '0755'
-  variables(
-    cmkserver: 'localhost',
-    apitoken: node['cmk']['secret'],
-    host_ip: 'localhost',
-    host_name: 'cmkpod-bento-almalinux-8.vagrantup.com'
-  )
-  sensitive true
-end
-
-# Discover host - curl
-template '/usr/local/bin/discoverhostc.sh' do
-  source 'discoverhostc.sh.erb'
-  mode '0755'
-  variables(
-    cmkserver: 'localhost',
-    apitoken: node['cmk']['secret'],
-    host_name: 'cmkpod-bento-almalinux-8.vagrantup.com'
-  )
-  sensitive true
-end
-
-# Activate changes - curl
-template '/usr/local/bin/activatec.sh' do
-  source 'activatec.sh.erb'
-  mode '0755'
-  variables(
-    cmkserver: 'localhost',
-    apitoken: node['cmk']['secret']
-  )
-  sensitive true
-end
-
 # Test to verify the Checkmk API
 execute 'test_api' do
   command '/usr/local/bin/listrulec.sh'
-  cwd '/usr/local/bin'
-  live_stream true
-  action :nothing
-end
-
-# Add host to Checkmk server - curl
-execute 'add_host' do
-  command '/usr/local/bin/addhostc.sh'
-  cwd '/usr/local/bin'
-  live_stream true
-  action :nothing
-end
-
-# Discover services on a host - curl
-execute 'discover_host' do
-  command '/usr/local/bin/discoverhostc.sh'
-  cwd '/usr/local/bin'
-  live_stream true
-  action :nothing
-end
-
-# Activate changes - curl
-execute 'activate_changes' do
-  command '/usr/local/bin/activatec.sh'
   cwd '/usr/local/bin'
   live_stream true
   action :nothing
